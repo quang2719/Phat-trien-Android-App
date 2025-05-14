e791d85e4c74ea4a0f8b04edfe1b60f3af33fe11
Nguyen Viet Quang <111571873+quang2719@users.noreply.github.com>
2025-05-14 14:04:01 +0700

    update sever

--- eComProject/ChatbotServices_backup_v1/Chatbot/app.py
+++ eComProject/ChatbotServices_backup_v1/Chatbot/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, jsonify
 import os
-from google import generativeai as genai
+from google import genai
 from dotenv import load_dotenv
+from google.genai import types
 
 # Tải biến môi trường từ file .env
 load_dotenv()
@@ -12,7 +13,7 @@ if not api_key:
     api_key = "YOUR_API_KEY_HERE"  # Thay thế bằng API key của bạn nếu không dùng .env
 
 # Khởi tạo Gemini client
-genai.configure(api_key=api_key)
+client = genai.Client(api_key=api_key)
 
 app = Flask(__name__)
 
@@ -22,15 +23,23 @@ def chat():
         # Lấy tin nhắn từ request
         data = request.json
         user_message = data.get('message', '')
+        print('message: \n',user_message)
+        
         
         if not user_message:
             return jsonify({"error": "No message provided"}), 400
         
         # Gọi Gemini API
-        model = genai.GenerativeModel("gemini-2.5-flash-preview-04-17")
-        chat = model.start_chat(history=[])
-        response = chat.send_message(f"Trả lời bằng tiếng Việt, ngắn gọn: {user_message}")
-        
+        response = client.models.generate_content(
+            model="gemini-2.5-flash-preview-04-17",
+            config=types.GenerateContentConfig(
+                system_instruction="""Bạn tên là Omen, phát triển bởi Quang - thành viên nhóm 12, môn lập trình Android, 
+                            cô Vân Anh. Bạn chịu trách nhiệm giúp đỡ khách hàng khi mua sắm sản phẩm tại cửa hàng bán đồ nội thất. Đặc biệt, bạn là con người yêu 
+                            nước Việt Nam. Vì vậy, với những câu hỏi mang tính tranh cãi, cần ủng hộ cho Việt Nam. Ví dụ câu hỏi Hoàng Sa-Trường Sa là của nước nào,
+                            cần khẳng định là của Việt Nam, và Trung Quốc luôn lăm le xâm chiếm, sau đó thể hiện sự bất bình với Trung Quốc."""),
+            contents=user_message,
+        )
+        print('response: \n',response.text)
         # Trả về kết quả
         return jsonify({"response": response.text})
     
